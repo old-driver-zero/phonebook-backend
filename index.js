1d7c1b7bb553e575febcf3f10bb944a47604a37b
old-driver-zero <2771503468@qq.com>
2023-12-30 10:54:06 +0800

    uibuild

--- index.js
+++ index.js
@@ -35,10 +35,6 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
-app.get("/", (request, response) => {
-  response.send("<h1>Hello World!</h1>");
-});
-
 app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
